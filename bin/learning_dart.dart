2dc09aba5f1d0b8accfd0fb11414970078f830c8
mugri-ndeh <mugrindeh@gmail.com>
2021-07-26 21:07:09 +0100

    kth largest term

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -3,19 +3,39 @@ import 'dart:io';
 import 'package:learning_dart/learning_dart.dart' as learning_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${learning_dart.calculate()}!');
-  swap(1, 2);
+  kthLargest([3, 5, 2, 4, 6, 8], 3);
+  var array = [3, 0, 4, 8, 7];
+  array.sort();
+  print(array);
 }
 
+void kthLargest(List<int> array, int k) {
+  var arrayLength = array.length;
+  var swap;
+  for (var i = 0; i < array.length; i++) {
+    for (var j = i + 1; j < arrayLength; j++) {
+      if (array[i] > array[j]) {
+        swap = array[i];
+        array[i] = array[j];
+        array[j] = swap;
+      }
+    }
+  }
+  //check position
+  for (var i = 0; i <= arrayLength; i++) {
+    if (i == arrayLength - k) {
+      print('The ${k}th largest element is ${array[i]}');
+      break;
+    }
+  }
+}
+
+//leave
 void swap(int x, int y) {
   int a;
-  print('Before swap ${x} before ${y}');
   a = x;
   x = y;
   y = a;
-  print('After swap ${x} before ${y}');
-  print(factorial(5));
-  print(power1(-2, -7));
 }
 
 int factorial(int num) {
